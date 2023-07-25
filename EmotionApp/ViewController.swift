7bf5754e100d19fc026298af79ce0b1832f8602f
Hyuk0510 <hyouk970510@gmail.com>
2023-07-25 16:13:18 +0900

    Initial Commit

---
+++ EmotionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmotionApp
+//
+//  Created by 선상혁 on 2023/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
